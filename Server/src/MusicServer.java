02fd249cb90bc32910955f53046cce0f4adc8506
vasekva <vahonin_vasek@mail.ru>
2020-02-17 11:48:07 +0300

    server for BeatBox

---
+++ Server/src/MusicServer.java
@@ -0,0 +1,72 @@
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.util.ArrayList;
+import java.util.Iterator;
+
+public class MusicServer {
+    ArrayList<ObjectOutputStream> clientOutputStreams;
+
+    public static void main(String[] args) {
+        new MusicServer().go();
+    }
+
+    public class ClientHandler implements Runnable {
+        ObjectInputStream in;
+        Socket clientSocket;
+
+        public ClientHandler(Socket socket) {
+            try {
+                clientSocket = socket;
+                in = new ObjectInputStream(clientSocket.getInputStream());
+            } catch(Exception e) { e.printStackTrace(); }
+        }
+
+        @Override
+        public void run() {
+            Object o2 = null;
+            Object o1 = null;
+            try {
+                while ( (o1 = in.readObject()) != null) {
+                    o2 = in.readObject();
+
+                    System.out.println("read two objects");
+                    tellEveryone(o1, o2);
+                }
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+
+    public void go() {
+        clientOutputStreams = new ArrayList<>();
+
+        try {
+            ServerSocket serverSock = new ServerSocket(4242);
+
+            while (true) {
+                Socket clientSocket = serverSock.accept();
+                ObjectOutputStream out = new ObjectOutputStream(clientSocket.getOutputStream());
+                clientOutputStreams.add(out);
+
+                Thread t = new Thread(new ClientHandler(clientSocket));
+                t.start();
+
+                System.out.println("got a connection");
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    public void tellEveryone(Object one, Object two) {
+        Iterator it = clientOutputStreams.iterator();
+        while (it.hasNext()) {
+            try {
+                ObjectOutputStream out = (ObjectOutputStream) it.next();
+                out.writeObject(one);
+                out.writeObject(two);
+            } catch (Exception e) { e.printStackTrace(); }
+        }
+    }
+}
